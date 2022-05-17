1f544b4ffc6c7cc640687018b8593a1ebc6b8b52
snehasamuel <snehasamuel297@gmail.com>
2022-05-17 12:26:02 +0530

    Search Book completed

--- src/components/Searchbook.js
+++ src/components/Searchbook.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 const Searchbook = () => {
   return (
     <div>
-        
+        <Navigation/>
+        <div classNameName="container">
+    <div classNameName="row">
+        <div classNameName="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
+<div className="row g-3">
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <label for="" className="form-label">Book Title</label>
+        <input type="text" className="form-control"/>
+    </div>
+    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+        <button className="btn btn-info">SEARCH</button>
+    </div>
+</div>
+
+        </div>
+    </div>
+</div>
         
     </div>
   )
