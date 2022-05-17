e8fdda538f6b5634c4f9feab0148ad231f0d7bd1
snehasamuel <snehasamuel297@gmail.com>
2022-05-17 12:14:12 +0530

    Header completed

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
   <Routes>
 <Route path="/" exact element={<Addbook/>}/>
 
+
   </Routes>
   </BrowserRouter>
 </div>    
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,9 +5,9 @@ const Navigation = () => {
   return (
     <div>
         
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-info">
   <div className="container-fluid">
-<Link className="navbar-brand" to="/">
+<Link className="navbar-brand" to="/">Books
 </Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
@@ -15,7 +15,8 @@ const Navigation = () => {
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+<Link className="nav-link active"  to="/">Add Books
+</Link>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Features</a>
